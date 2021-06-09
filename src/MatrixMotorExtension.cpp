49387482e3d492390167f3ccb14075315ef58889
DCisHurt <46733725+DCisHurt@users.noreply.github.com>
2021-06-09 18:14:31 +0800

    Add files via upload

---
+++ src/MatrixMotorExtension.cpp
@@ -0,0 +1,103 @@
+#include "MatrixMotorExtension.h"
+
+bool MatrixMotor::begin(uint8_t ver, uint8_t ch){
+	Wire.begin();
+	_ver = ver;
+	_ch = ch;
+	i2cMUXSelect();
+	delay(50);
+	if(i2cReadData(Device_ID) == 0x45){
+		i2cWriteData(Device_Control_1, 0x08); // reset
+		delay(500);
+		i2cWriteData(Device_Control_1, 0x00); // module enable
+		i2cWriteData(Device_Control_2, 0xF0); // channel enable
+		delay(500);
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+float MatrixMotor::getVbat(){
+	return i2cReadData(Battery_Voltage)*0.033;
+}
+
+void MatrixMotor::setPWM(MotorRegType ch, int speed){
+	i2cMUXSelect();
+	pwmChannel = ch;
+	if(speed < 0){
+		speed *= -1;
+		channelReverse(1);
+	}
+	else{
+		channelReverse(0);
+	}
+	i2cWriteData(ch, speed);
+}
+
+void MatrixMotor::channelReverse(uint8_t dir){
+
+	switch (pwmChannel){
+		case CH1_PWM:
+			_reverse1 = dir; 
+			break;
+		case CH2_PWM:
+			_reverse2 = dir; 
+			break;
+		case CH3_PWM:
+			_reverse3 = dir; 
+			break;
+		case CH4_PWM:
+			_reverse4 = dir; 
+			break;
+	}
+	i2cWriteData(Device_Control_2,  0xF0 + (_reverse1 << 3) + (_reverse2 << 2) + (_reverse3 << 1) + _reverse4);
+}
+
+uint8_t MatrixMotor::i2cReadData(MotorRegType reg){
+	
+	Wire.beginTransmission(MatrixMotor_ADDR);
+	Wire.write(reg);
+	Wire.endTransmission(1);
+
+	delay(1);
+
+	Wire.requestFrom(MatrixMotor_ADDR, 1);
+
+	delay(1);
+
+	return Wire.read();
+}
+
+void MatrixMotor::i2cMUXSelect(){
+	switch (_ver)
+	{
+	case 2:
+		Wire.beginTransmission(ADDR_PCA954X);
+		Wire.write(_ch + 4);
+		Wire.endTransmission(1);
+
+		delay(10);
+		break;
+	case 3:
+		Wire.beginTransmission(ADDR_PCA954X);
+		Wire.write((1 << _ch));
+		Wire.endTransmission(1);
+		
+		delay(10);
+		break;
+	default:
+		break;
+	}
+}
+
+void MatrixMotor::i2cWriteData(MotorRegType reg, uint8_t data){
+
+	Wire.beginTransmission(MatrixMotor_ADDR);
+
+	Wire.write(reg);
+	Wire.write(data);
+
+	Wire.endTransmission(1);
+}
---
+++ src/MatrixMotorExtension.h
@@ -0,0 +1,39 @@
+#ifndef _MatrixMotorExtension_H_
+#define _MatrixMotorExtension_H_
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#define ADDR_PCA954X 0x70
+
+#define MatrixMotor_ADDR 0x24
+
+typedef enum __MotorRegType
+{
+  Device_ID = 1,
+  Device_Control_1,
+  Device_Control_2, 
+  Battery_Voltage,
+  CH1_PWM,
+  CH2_PWM,
+  CH3_PWM,
+  CH4_PWM
+} MotorRegType;
+
+class MatrixMotor{
+private:
+	uint8_t _ch, _ver;
+  uint8_t _reverse1 = 0, _reverse2 = 0, _reverse3 = 0, _reverse4 = 0;
+  uint8_t pwmChannel;
+	uint8_t i2cReadData(MotorRegType reg);
+	void i2cMUXSelect();
+	void i2cWriteData(MotorRegType reg, uint8_t data);
+  void channelReverse(uint8_t dir);
+public:
+	friend class MiniI2C;
+	bool begin(uint8_t ver=1, uint8_t ch=0);
+	float getVbat();
+	void setPWM(MotorRegType ch, int speed);
+};
+
+#endif
